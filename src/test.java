b188930755a9211bb94a68b8434385e14cb076df
Thura Linn <thuralinn45512@gmail.com>
2022-01-31 16:13:20 +0630

    "third commit"

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test.main");
         System.out.println("Hello ");
+        System.out.println("Third output");
     }
 }
